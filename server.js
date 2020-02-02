dce79cdd06f14b1f3eca9bfb6f02820c847d03fb
Sailaja Nagireddy <snagireddy@agnosy.org>
2020-02-02 08:32:21 +0530

    - Added hello vue. - Updated nuxt configuration and server script.

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const { Nuxt, Builder } = require('nuxt')
 const express = require('express')
-const config = require('./nuxt.config')
+// const config = require('./nuxt.config')
+/*
+const config = {
+  dev: false,
+  buildDir: nuxt,
+  build: {
+    publicPath: '/public/'
+  }
+}
+*/
+
+const config = {
+  dev: false,
+  debug: false
+}
 
 const dev = process.env.NODE_ENV !== 'production'
 
-const nuxt = new Nuxt({ ...config, dev })
+// const nuxt = new Nuxt({ ...config, dev })
+const nuxt = new Nuxt(config)
 
 if (dev) {
   const builder = new Builder(nuxt)
